cdce4f08672b472eec1cf6574b7e332ac0009925
jingliliujp <jingliliujp@gmail.com>
2024-10-05 12:24:03 +0900

    dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jingliliujp/puppy
 
 go 1.22.5
+
+require github.com/jingliliujp/dog v0.0.0-20241005022057-f70daf6c3b28 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/jingliliujp/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrounUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrounUp(Barks())
 }
